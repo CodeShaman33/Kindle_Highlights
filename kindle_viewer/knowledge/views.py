c13b0c6a0d27bc0572980bc125385612eab81399
PsychoShaman <111572177+PsychoShaman@users.noreply.github.com>
2022-11-22 18:52:48 +0100

    crete view for homepage, create url pattern, create base.html which will be extended by every other page on this web app

--- kindle_viewer/knowledge/views.py
+++ kindle_viewer/knowledge/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponseRedirect, HttpResponse
+from django.core.files import File
+from django.core.files.storage import FileSystemStorage
+'''models '''
+from .models import Book, Document, HighLight
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'kindle/index.html')

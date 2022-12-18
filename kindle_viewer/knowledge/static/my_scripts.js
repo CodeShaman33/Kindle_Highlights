74c6b4563f82f980f05ea4274102f32e1cf7112f
PsychoShaman <111572177+PsychoShaman@users.noreply.github.com>
2022-12-18 11:33:39 +0100

    add js screens to the main page

--- kindle_viewer/knowledge/static/my_scripts.js
+++ kindle_viewer/knowledge/static/my_scripts.js
@@ -1,5 +1,10 @@
 
 
-function userNote(){
-        document.getElementById("demo").style.color = 'red';
-    }
+function screens(){
+    let image = document.getElementById('photos');
+    let images = ['static/image1.JPG','static/image2.JPG','static/image3.JPG','static/kindle.jpg']
+    setInterval(function(){
+        let random = Math.floor(Math.random() * 4);
+        image.src = images[random];
+    }, 800);
+}
